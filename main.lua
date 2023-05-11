af553923e6557d9f3e32a4d45983599aeed20910
cpiod <cpiod@pm.me>
2023-05-11 23:58:42 +0200

    Selection UI (WIP)

--- main.lua
+++ main.lua
@@ -19,18 +19,23 @@ function get_exotic( item )
     end
     result.count = count
 
-    assembly = {
+    -- TODO a single item can participate in multiple recipes
+    -- TODO handle trait level
+    -- TODO handle other requirements (heart, relic, multitool, etc.)
+    local assembly = {
         { base = "pistol", new = "exo_mpistol", P = 1 },
         { base = "armor_green", new = "exo_armor_ablative", P = 1},
         { base = "helmet_green", new = "exo_helmet_blast", P = 1 },
     }
-    mods = {"P","A","B","S","V","C","E","O","N"}
+    local mods = {"P","A","B","S","V","C","E","O","N"}
     for _,v in ipairs(assembly) do
         if world:get_id(item) == v.base then
             local good = true
+            -- check all requirements
             for _,mod in ipairs(mods) do
                 if v[mod] and not (result[mod] and result[mod] >= v[mod]) then
                     good = false
+                    break
                 end
             end
             if good then
@@ -42,7 +47,44 @@ function get_exotic( item )
     return nil
 end
 
+function run_assembly_ui( self, entity )
+    local list = {}
+    local max_len = 1
+    local slots = {"1","2","3","4","armor","head"}
+    for _,slot in ipairs(slots) do
+        local item = world:get_slot( entity, slot )
+        if item then
+            local exo = get_exotic(item)
+            if exo then
+                local name = world:get_name(item).." {!=>}  "..world:get_text(exo,"name")
+                max_len = math.max( max_len, string.len( name ) )
+                table.insert(list, {
+                    name = name,
+                    target = self,
+                    parameter = {item = item, new = exo}
+                })
+            end
+        end
+    end
 
+
+    if #list == 0 then
+        ui:set_hint( "{RNothing to assemble!}", 1001, 0 )
+        return
+    end
+
+    table.insert( list, {
+        name = "Cancel",
+        target = self,
+        cancel = true,
+    })
+    list.title = "What to assemble?"
+    list.size  = coord( math.max( 30, max_len + 6 ), 0 )
+    ui:terminal( entity, nil, list )
+end
+
+-- TODO: requirement (whizkid ?)
+-- TODO: vérifier si assembly possible ?
 register_blueprint "trait_assembly"
 {
     blueprint = "trait",
@@ -51,8 +93,6 @@ register_blueprint "trait_assembly"
         desc   = "",
         full   = "",
         abbr   = "",
-        denied = "{RNothing to assemble!}",
-        assembly = "You assembled a ",
     },
     callbacks = {
         on_activate = [=[
@@ -60,25 +100,37 @@ register_blueprint "trait_assembly"
                 return -1
             end
         ]=],
+
         on_use = [=[
-            function ( self, player, level, target )
-                slots = {"1","2","3","4","armor","head"}
-                for _,slot in pairs(slots) do
-                    item = world:get_slot( player, slot )
-                    if item then
-                        exo = get_exotic(item)
-                        if exo then
-                            world:play_voice("vo_unique")
-                            ui:set_hint("Bye {!"..world:get_name(item).."}, hello {!"..world:get_text(exo,"name").."}!", 2001, 0)
-                            level:drop_item( player, item )
-                            world:destroy(item)
-                            -- TODO: apply manufacturer perk?
-                            player:pickup( exo, true )
-                            return -1
-                        end
-                    end
+            function( self, entity )
+                if entity == world:get_player() then
+                    run_assembly_ui( self, entity )
+                    return -1
+                else 
+                    return -1
                 end
-                ui:set_hint( self.text.denied, 1001, 0 )
+            end
+        ]=],
+        on_activate = [=[
+            function ( self, player, level, param )
+                nova.log("ASSEMBLY - on activate called")
+                if param then
+                    item = param.item
+                    exo = param.new
+                    nova.log("item:"..item)
+                    nova.log("exo:"..exo)
+                    item = list[1].item
+                    exo = list[1].new
+                    world:play_voice("vo_unique")
+                    -- ui:set_hint("Bye {!"..world:get_name(item).."}, hello {!"..world:get_text(exo,"name").."}!", 2001, 0)
+                    level:drop_item( player, item )
+                    world:destroy(item)
+                    -- TODO: apply manufacturer perk?
+                    player:pickup( exo, true )
+                    return 99
+                else
+                    return 0
+                end
             end
         ]=],
     },
