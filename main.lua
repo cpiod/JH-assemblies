b0c0abb07a4315a8f5511658a35df6a3034b38c0
cpiod <cpiod@pm.me>
2023-05-18 00:04:04 +0200

    Recipes

--- main.lua
+++ main.lua
@@ -1,18 +1,37 @@
 classes = { "klass_marine", "klass_scout", "klass_technician" }
 
-for _,k in ipairs(classes) do
-    table.insert(blueprints[k].klass.traits, #blueprints[k].klass.traits-4, { "trait_assembly", max = 3, require = { trait_whizkid = 1, } })
-end
+table.insert(blueprints["klass_marine"].klass.traits, #blueprints["klass_marine"].klass.traits-4, { "trait_assembly", max = 3, require = { ktrait_army_surplus = 1, } })
+table.insert(blueprints["klass_scout"].klass.traits, #blueprints["klass_scout"].klass.traits-4, { "trait_assembly", max = 3, require = { trait_hacking = 1, } })
+table.insert(blueprints["klass_technician"].klass.traits, #blueprints["klass_technician"].klass.traits-4, { "trait_assembly", max = 3, require = { trait_whizkid = 1, } })
 
--- whizkid 1 necessary, cost 1 multitool
+-- cost 1 multitool
 assembly_l1 = {
     { base = "knife", new = "exo_knife", A = 1, P = 1 },
-    { base = "rpistol", new = "exo_fpistol", A = 2 }, -- flintlock
-    { base = "smg", new = "exo_ssmg", B = 2 }, -- storm
-    { base = "hunter_rifle", new = "exo_toxi_rifle", P = 2 },
+    { base = "bpistol", new = "exo_blaster", A = 1, P = 1 },
+    { base = "hunter_rifle", new = "exo_toxi_rifle", P = 1, B = 1 },
     { base = "auto_rifle", new = "exo_nailgun", B = 1, P = 1 },
-    { base = "shotgun", new = "exo_cshotgun", A = 2 }, -- focused
-    { base = "armor_green", new = "exo_armor_necrotic", B = 1, A = 1 }, -- any armor ?
+    { base = "armor_green", new = "exo_armor_duramesh", B = 1, A = 1 },
+
+    -- yellow to red
+    { base = "pistol", new = "apistol", B = 1, A = 1 }, -- pistol to 7.62 sidearm
+    { base = "pistol", new = "bpistol", B = 1, A = 1 }, -- pistol to combat pistol
+    { base = "rpistol", new = "lpistol", B = 1, A = 1 }, -- revolver to long revolver
+    { base = "smg", new = "asmg", B = 1, A = 1 }, -- SMG to 7.62 SMG
+    { base = "smg", new = "esmg", B = 1, A = 1 }, -- SMG to plasma SMG
+    { base = "hunter_rifle", new = "long_rifle", B = 1, A = 1 },
+    { base = "hunter_rifle", new = "rail_rifle", B = 1, A = 1 },
+    { base = "chaingun", new = "hyperblaster", B = 1, A = 1 },
+    { base = "shotgun", new = "dshotgun", B = 1, A = 1 }, -- 12ga shotgun to dual shotgun
+    { base = "ashotgun", new = "dshotgun", B = 1, A = 1 }, -- auto-shotgun to dual shotgun
+    { base = "rocket_launcher", new = "energy_cannon", B = 1, A = 1 },
+
+    -- grenades
+    { base = "ammo_40", new = "smoke_grenade", base_amount = 15 },
+    { base = "ammo_40", new = "frag_grenade", base_amount = 15 },
+    { base = "ammo_40", new = "krak_grenade", base_amount = 15 },
+    { base = "ammo_40", new = "emp_grenade", base_amount = 15 },
+    { base = "ammo_40", new = "gas_grenade", base_amount = 15 },
+    { base = "ammo_40", new = "napalm_grenade", base_amount = 15 },
 }
 
 for _,v in ipairs(assembly_l1) do
@@ -20,43 +39,111 @@ for _,v in ipairs(assembly_l1) do
 end
 -- TODO: add recipes with AMP
 
--- whizkid 2 necessary, cost 2 multitools and a relic
+-- cost 2 multitools and a relic
 assembly_l2 = {
     { base = "exo_egls", new = "exo_egls" }, -- "reload" EGLS
     { base = "exo_mag_rifle", new = "exo_mag_rifle" }, -- "reload" railgun
-    { base = "exo_mpistol", new = "exo_mpistol" }, -- "reload" mag pistol
-    { base = "exo_nailgun", new = "exo_snailgun", P = 1 },
-    { base = "rocket_launcher", new = "exo_micro_launcher", P = 1 },
-    { base = "helmet_blue", new = "exo_helmet_blast", B = 2, P = 1 },
+    { base = "exo_armor_ablative", new = "exo_armor_ablative" }, -- "repair" ablative
+    { base = "exo_nailgun", new = "exo_snailgun" },
+    { base = "rocket_launcher", new = "exo_toxin_launcher", AV = 1 },
+    { base = "helmet_blue", new = "exo_helmet_blast", AV = 1 },
+    { base = "medkit_large", new = "combatpack_large" },
 }
+
 for _,v in ipairs(assembly_l2) do
     v.mt = 2
     v.relic = 1
 end
 
--- whizkid 2 necessary, cost 2 multitools, the heart and 40 HP
+-- cost the heart and 10 max HP
 assembly_l3 = {
     -- tier 1 -> tier 2, tier 2 -> tier 3 weapon of the same type, requires heart so cannot be cumulated with dark cathedral
     { base = "uni_revolver_love", new = "uni_pistol_hate" },
     { base = "uni_pistol_hate", new = "uni_pistol_death" },
--- TODO: complete
-    { base = "pack_power", new = "powerup_backpack" }, -- maybe permanent_backpack ?
-    { base = "pack_bulk", new = "adv_pack_sustain" },
-    { base = "adv_pack_sustain", new = "exo_pack_nano" },
-    { base = "pack_bulk", new = "exo_pack_onyx" },
-    { base = "armor_red", new = "armor_cri", A = 1, B = 1, P = 1 },
-    { base = "relic_major", new = "relic_medusa_eye" },
-    { base = "relic_major", new = "relic_medusa_tentacle" },
+    { base = "uni_rifle_thompson", new = "uni_rifle_hammerhead" },
+    { base = "uni_rifle_hammerhead", new = "uni_rifle_avalanche" },
+    { base = "uni_semi_vengeance", new = "uni_semi_bloodletter" },
+    { base = "uni_semi_bloodletter", new = "uni_semi_shadowhunter" },
+    { base = "uni_smg_carnage", new = "uni_smg_viper" },
+    { base = "uni_smg_viper", new = "uni_smg_void" },
+    { base = "uni_shotgun_monster", new = "uni_shotgun_denial" },
+    { base = "uni_shotgun_denial", new = "uni_shotgun_wavedancer" },
+    { base = "uni_knife", new = "uni_katana" },
+    { base = "uni_katana", new = "uni_sword" },
+    { base = "uni_launcher_firestorm", new = "uni_launcher_calamity" },
+    { base = "uni_launcher_calamity", new = "uni_bfg" },
+    { base = "uni_scrapgun", new = "uni_vulcan" },
+    { base = "uni_vulcan", new = "uni_apocalypse" },
+
+    -- upgrade exo to tier 1 unique
+    { base = "exo_saw", new = "uni_knife" },
+    { base = "exo_knife", new = "uni_knife" },
+    { base = "exo_sword", new = "uni_knife" },
+    { base = "exo_ksword_marine", new = "uni_knife" },
+    { base = "exo_ksword_scout", new = "uni_knife" },
+    { base = "exo_ksword_tech", new = "uni_knife" },
+    { base = "exo_katana", new = "uni_knife" },
+    { base = "exo_ancient_sword", new = "uni_knife" },
+
+    { base = "exo_cpistol", new = "uni_revolver_love" },
+    { base = "exo_mpistol", new = "uni_revolver_love" },
+    { base = "exo_dpistol", new = "uni_revolver_love" },
+    { base = "exo_blaster", new = "uni_revolver_love" },
+    { base = "exo_jpistol", new = "uni_revolver_love" },
+    { base = "exo_fpistol", new = "uni_revolver_love" },
+
+    { base = "exo_tsmg", new = "uni_smg_carnage" },
+    { base = "exo_jsmg", new = "uni_smg_carnage" },
+    { base = "exo_csmg", new = "uni_smg_carnage" },
+    { base = "exo_ssmg", new = "uni_smg_carnage" },
+
+    { base = "exo_toxi_rifle", new = "uni_semi_vengeance" },
+    { base = "exo_awp_rifle", new = "uni_semi_vengeance" },
+    { base = "exo_mag_rifle", new = "uni_semi_vengeance" },
+    { base = "exo_emp_rifle", new = "uni_semi_vengeance" },
+    { base = "exo_railgun", new = "uni_semi_vengeance" },
+
+    { base = "exo_snailgun", new = "uni_scrapgun" },
+    { base = "exo_gatling_gun", new = "uni_scrapgun" },
+
+    { base = "exo_cshotgun", new = "uni_shotgun_monster" },
+    { base = "exo_fshotgun", new = "uni_shotgun_monster" },
+    { base = "exo_gshotgun", new = "uni_shotgun_monster" },
+    { base = "exo_jshotgun", new = "uni_shotgun_monster" },
+    { base = "exo_sshotgun", new = "uni_shotgun_monster" },
+
+    { base = "exo_egls", new = "uni_launcher_firestorm" },
+    { base = "exo_grenade_launcher", new = "uni_launcher_firestorm" },
+    { base = "exo_micro_launcher", new = "uni_launcher_firestorm" },
+    { base = "exo_toxin_launcher", new = "uni_launcher_firestorm" },
+    { base = "exo_bfg", new = "uni_launcher_firestorm" },
+
+    { base = "exo_tac_rifle", new = "uni_rifle_thompson" },
+    { base = "exo_precision_rifle", new = "uni_rifle_thompson" },
+    { base = "exo_ac_rifle", new = "uni_rifle_thompson" },
+    { base = "exo_nailgun", new = "uni_rifle_thompson" },
+    { base = "exo_ancient_gun", new = "uni_rifle_thompson" },
+
+    { base = "adv_amp_general", new = "powerup_backpack" }, -- maybe permanent_backpack ?
+    { base = "exo_ancient_gun", new = "exo_pack_nano" },
+    { base = "exo_ancient_gun", new = "exo_pack_onyx" },
+    { base = "exo_ancient_sword", new = "exo_pack_nano" },
+    { base = "exo_ancient_sword", new = "exo_pack_onyx" },
+    { base = "ancient_relic_ancient_necklace", new = "exo_pack_nano" },
+    { base = "ancient_relic_ancient_necklace", new = "exo_pack_onyx" },
+    { base = "ancient_relic_ancient_armband", new = "exo_pack_nano" },
+    { base = "ancient_relic_ancient_armband", new = "exo_pack_onyx" },
+
 }
 for _,v in ipairs(assembly_l3) do
-    v.hp = 40
+    v.max_hp = 10
     v.heart = true
 end
 
 assembly_test = {
-    { base = "base_weapon", new = "exo_mpistol", P = 1 },
-    { base = "pistol", new = "exo_cpistol", P = 1 },
-    { base = "armor_green", new = "exo_armor_ablative", P = 1, heart = true},
+    { base = "base_weapon", new = "exo_mpistol", P = 1, max_hp = 20 },
+    { base = "pistol", new = "exo_nailgun", P = 1 },
+    { base = "armor_green", new = "exo_armor_ablative", P = 1, heart = true, max_hp = 20},
     { base = "helmet_green", new = "exo_helmet_blast", P = 1, heart = true },
 }
 for _,v in ipairs(assembly_test) do
@@ -71,7 +158,7 @@ function can_pay(player, base, new)
             return not ((v.mt and world:has_item( player, "kit_multitool" ) < v.mt) or
             (v.heart and world:has_item( player, "frozen_heart") == 0) or
             (v.relic and not world:get_slot( player, "relic" )) or
-            (v.hp and player.health.current <= v.hp))
+            (v.max_hp and player.health.current <= v.max_hp))
         end
     end
 end
@@ -92,8 +179,9 @@ function pay_cost(player, item, new)
                 level:drop_item(player, r)
                 world:destroy(r)
             end
-            if v.hp then
-                level:apply_damage( player, player, v.hp, ivec2(), "internal" )
+            if v.max_hp then
+                level:apply_damage( player, player, v.max_hp, ivec2(), "internal" )
+                player.attributes.health = player.attributes.health - v.max_hp
             end
         end
     end
@@ -151,6 +239,7 @@ function run_assembly_ui( self, entity )
         if item then
             local recipes = get_recipes(entity, item)
             for _,recipe in ipairs(recipes) do
+                -- put assembly level
                 local name = world:get_name(item).." {Y=>} "..world:get_text(recipe.new,"name")
                 max_len = math.max( max_len, string.len( name ) )
                 table.insert(list, {
@@ -178,34 +267,35 @@ function run_assembly_ui( self, entity )
     ui:terminal( entity, what, list )
 end
 
--- TODO: requirement (whizkid ?)
 register_blueprint "trait_assembly"
 {
     blueprint = "trait",
     text = {
-        name   = "Assembler",
-        desc   = "ACTIVE SKILL - Assemble new items from existing ones",
+        name   = "Assembler",-- indicate current number of availables assemblies
+        desc   = "ACTIVE SKILL - You make new toys from old ones. Manufacturer perks are kept!",
         full   = [[{!LEVEL 1} - Cost: {!1 multitool}
-AP combat knife   {Y=>} quickblade
-B2 9mm SMG        {Y=>} 9mm storm
-P2 .44 revolver   {Y=>} .44 flintlock
-P2 hunter rifle   {Y=>} toxin rifle
-PB auto rifle     {Y=>} nailgun
-A2 12ga shotgun   {Y=>} focused shotgun
-AB green armor    {Y=>} necrotic armor
+ AP combat knife   => quickblade
+ AP combat pistol  => CRI blaster
+ BP hunter rifle   => toxin rifle
+ BP 9mm auto rifle => nail gun
+ AB green armor    => duramesh armor
+ AB yellow weapon  => red weapon{!*}
+ 15 40mm grenades  => non-plasma grenade
 {!LEVEL 2} - Cost: {!2 multitools}, {!1 relic}
-Reload EGLS/magrail - Restore mag-pistol
-P nailgun         {Y=>} super nailgun
-P R. Launcher     {Y=>} Micro launcher
-BBP blue helmet   {Y=>} Blast helmet
-{!LEVEL 3} - Cost: {!40 HP}, a {!frozen heart}
-Upgrade unique to next tier
-P mod             {Y=>} Backpack
-A mod             {Y=>} S mod
-B mod             {Y=>} O mod
-ABP red armor     {Y=>} CRI armor]],
+ restore magrail / EGLS / ablative armor
+ nail gun          => super nailgun
+ AV1+ rocket laun. => bio launcher
+ AV1+ blue helmet  => blast helmet
+ large medkit      => large combat pack
+{!LEVEL 3} - Cost: {!-10 max HP}, {!frozen heart}
+ exotic item       => tier 1 unique{!*}
+ unique item       => next tier unique{!*}
+ utility AMP       => CRI backpack
+ Ancient’s drop    => O or N mod pack
+  {!*} item type is preserved]],
         abbr   = "Asm",
     },
+ -- Level 5+ Rexio => Golden Gun ???
     callbacks = {
 
         on_use = [=[
@@ -221,16 +311,20 @@ ABP red armor     {Y=>} CRI armor]],
 
         on_activate = [=[
             function ( self, player, level, param, id )
-                if param then
-                    local item = param
-                    local new = world:resolve_hash( id )
-                    pay_cost(player,item, new)
-                    world:play_voice("vo_unique")
-                    level:drop_item( player, item )
-                    world:destroy(item)
-                    -- TODO: apply manufacturer perk?
-                    player:pickup( new, true )
-                    return 100
+                if level then
+                    if param then
+                        local item = param
+                        local new = world:resolve_hash( id )
+                        pay_cost(player,item, new)
+                        world:play_voice("vo_unique")
+                        level:drop_item( player, item )
+                        world:destroy(item)
+                        -- TODO: apply manufacturer perk?
+                        player:pickup( new, true )
+                        return 100
+                    end
+                else
+                    player.attributes.assembly_level = ( player.attributes.assembly_level or 0 ) + 1
                 end
                 return 0
             end
@@ -271,6 +365,7 @@ register_blueprint "assembly_mod"
                 player:attach( "pistol" )
                 player:attach("relic_fiend_heart")
                 player:attach( "kit_multitool", { stack = { amount = 3 } } )
+                -- world:add_experience( player, 10000 )
             end
         ]],
     },
